04bd89deba1fe6ecb6380fe2c2e5323bcbb3c649
Fohristiwhirl-Skaffen-Amtiskaw <112807626+Fohristiwhirl-Skaffen-Amtiskaw@users.noreply.github.com>
2023-04-07 00:30:55 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using BigBearPlastics;
+
+namespace LineServiceSimulator
+{
+    internal class Program
+    {
+        static void Main(string[] args) {
+
+            List<ISimulatableMachine> machines = Factory.CreateAllMachines();
+            IServicer servicer = Factory.GetServicer();
+
+            for (int runTime = EightHours(); runTime > 0; runTime--) {
+                machines.ForEach(machine => {
+                    machine.Tick();
+                });
+                servicer.Tick();
+            }
+
+            machines.ForEach(machine => {
+                machine.LogMessage($"Uptime {(int)((machine.Uptime/(double)EightHours())*100)}%");
+            });
+
+            Console.WriteLine($"FLT: Uptime {(int)((servicer.Uptime/(double)EightHours())*100)}%");
+        }
+
+        static int EightHours() {
+            return 8 * 60 * 60;
+        }
+    }
+}
