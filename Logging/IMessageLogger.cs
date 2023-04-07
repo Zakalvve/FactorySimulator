012e961e16acd95f19d2df211cbe9a005830cac6
Fohristiwhirl-Skaffen-Amtiskaw <112807626+Fohristiwhirl-Skaffen-Amtiskaw@users.noreply.github.com>
2023-04-07 16:48:43 +0100

    An object used to log messages. A prefix may be supplied to identify the source of the message.

---
+++ Logging/IMessageLogger.cs
@@ -0,0 +1,9 @@
+﻿namespace BigBearPlastics
+{
+    public interface IMessageLogger
+    {
+        public string Prefix { get; set; }
+        public void LogMessage(string message);
+        public void LogSignedMessage(string message);
+    }
+}
---
+++ Logging/MessageLogger.cs
@@ -0,0 +1,15 @@
+﻿namespace BigBearPlastics
+{
+    public class MessageLogger : IMessageLogger
+    {
+        public string Prefix { get; set; } = "";
+
+        public void LogMessage(string message) {
+            Console.WriteLine(message);
+        }
+
+        public void LogSignedMessage(string message) {
+            if (String.IsNullOrEmpty(Prefix)) LogMessage(message); else Console.WriteLine($"{Prefix} {message}");
+        }
+    }
+}
