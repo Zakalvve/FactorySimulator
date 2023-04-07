edf96652abca03434f6294a798babd1e753ba373
Fohristiwhirl-Skaffen-Amtiskaw <112807626+Fohristiwhirl-Skaffen-Amtiskaw@users.noreply.github.com>
2023-04-07 16:40:19 +0100

    Moved interface to Machines folder.

--- Machines/IServicableMachine.cs
+++ Machines/IServicableMachine.cs
@@ -1,7 +1,10 @@
-﻿namespace BigBearPlastics
+﻿using BigBearPlastics;
+
+namespace LineServiceSimulator.Machines
 {
     public interface IServicableMachine
     {
+        //TODO: Tight coupling of machine to servicer. These classes shouldn't need to know about one another and the request could be raised as an event instead.
         public IServicer Servicer { get; set; }
         public void Request(List<ServiceRequest> requests);
         public ICommand ReplaceInputContainerCommand();
