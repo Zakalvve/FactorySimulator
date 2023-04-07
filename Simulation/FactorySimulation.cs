779bb4afae2004aeb0b6283863e43839c7239631
Fohristiwhirl-Skaffen-Amtiskaw <112807626+Fohristiwhirl-Skaffen-Amtiskaw@users.noreply.github.com>
2023-04-07 16:56:17 +0100

    Created Factory Simulation class. It's function is to asynchronously execute the simulation. Provide a progress report to the caller and record simulation results data which is returned upon completion.

---
+++ Simulation/FactorySimulation.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BigBearPlastics
+{
+    public class FactorySimulation
+    {
+        private int _simDuration;
+        public FactorySimulation(int duration/*More sim settings could go here (sim settings model?)*/) {
+            _simDuration = duration;
+        }
+        public Task Simulate() {
+            return Task.Run(Simulation);
+        }
+        private List<IReportingSimulatable> InitializeSimulation() {
+            List<IReportingSimulatable> sims = Factory.CreateAllMachines().Cast<IReportingSimulatable>().ToList();
+            sims.Add(Factory.GetServicer());
+            return sims;
+        }
+        private void Simulation() {
+
+            List<IReportingSimulatable> sims = InitializeSimulation();
+
+            for (int runTime = _simDuration; runTime > 0; runTime--) {
+                sims.ForEach(sim => {
+                    sim.Tick();
+                });
+            }
+
+            sims.ForEach(sim => {
+                sim.CloseReport(_simDuration);
+            });
+        }
+    }
+}
