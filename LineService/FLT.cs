cd80b65e5c3ba50af2cdb6e9f7753be17f2253e0
Fohristiwhirl-Skaffen-Amtiskaw <112807626+Fohristiwhirl-Skaffen-Amtiskaw@users.noreply.github.com>
2023-04-07 16:47:52 +0100

    Added IMessageLogger dependancy. Changes constrcutor signature. Now implements ISimulatableServicer.

--- LineService/FLT.cs
+++ LineService/FLT.cs
@@ -1,28 +1,34 @@
 ﻿namespace BigBearPlastics
 {
-    public class FLT : IServicer
+    //SRP: Should accept requests and expose methods to comply with ISimulatable
+    //It simulates the accepting and completing of requests from machines and simulates time it takes to perform those actions
+    public class FLT : ISimulatableServicer
     {
-        private ServiceRequest? _currentJob;
+        public int Uptime { get; set; }
+
+        private IMessageLogger _logger;
+        private ServiceRequest? _currentJob = null;
+        private PriorityQueue<ServiceRequest,int> _jobs;
         private int _timeSinceStartJob = 0;
 
-        private PriorityQueue<ServiceRequest,int> _jobs;
-        public FLT(PriorityQueue<ServiceRequest,int> jobs) {
+        public FLT(PriorityQueue<ServiceRequest,int> jobs, IMessageLogger logger) {
             _jobs = jobs;
+            _logger = logger;
+            _logger.Prefix = $"FLT:";
         }
         public bool AcceptRequest(int priority,List<ServiceRequest> requests) {
             try {
+                //add stratergy to determine calculated priority
                 requests.ForEach(request => {
                     _jobs.Enqueue(request,priority);
-                    LogMessage($"Accepted request at priority {priority} from CNC{request.MachineID}");
+                    _logger.LogSignedMessage($"Accepted request at priority {priority} from CNC{request.MachineID}");
                 });
-                LogMessage($"Requests in queue {_jobs.Count}");
+                _logger.LogSignedMessage($"Requests in queue {_jobs.Count}");
                 return true;
             } catch (Exception) {
                 return false;
             }
         }
-
-        public int Uptime { get; set; }
 
         public void Record() {
             throw new NotImplementedException();
@@ -34,7 +40,7 @@
                 Uptime++;
                 if (_timeSinceStartJob > _currentJob.ResponseTime) {
                     //fullfill the request
-                    LogMessage($"Resolved request \"{_currentJob.Name}\" for CNC{_currentJob.MachineID}. Response time {(int)(_timeSinceStartJob / 60D)} minutes");
+                    _logger.LogSignedMessage($"Resolved request \"{_currentJob.Name}\" for CNC{_currentJob.MachineID}. Response time {(int)(_timeSinceStartJob / 60D)} minutes");
                     _currentJob.Command.Execute();
                     _timeSinceStartJob = 0;
                     _currentJob = null;
@@ -43,14 +49,14 @@
             else {
                 if (_jobs.Count > 0) {
                     _currentJob = _jobs.Dequeue();
-                    LogMessage($"Picking up next job: \"{_currentJob.Name}\". Requested by CNC{_currentJob.MachineID}");
-                    LogMessage($"Requests in queue {_jobs.Count}");
+                    _logger.LogSignedMessage($"Picking up next job: \"{_currentJob.Name}\". Requested by CNC{_currentJob.MachineID}");
+                    _logger.LogSignedMessage($"Requests in queue {_jobs.Count}");
                 }
             }
         }
 
-        public void LogMessage(string message) {
-            Console.WriteLine($"FLT: {message}");
+        public void CloseReport(int simDuration) {
+            _logger.LogSignedMessage($"Uptime {(int)((Uptime / (double)(simDuration)) * 100)}%");
         }
     }
 }
